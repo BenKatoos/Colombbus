e2c48831db8ab05d61da0183636711ab66775365
BenKatoos <122570122+BenKatoos@users.noreply.github.com>
2023-03-02 17:02:00 +0100

    Update 02_selecteurs.js

--- JS_cours/DOM/02_selecteurs.js
+++ JS_cours/DOM/02_selecteurs.js
@@ -7,7 +7,7 @@ let select1 = titreNiveau1;
 console.log(select1);
 
 //////-------------------------------------------------------
-// Première méthode || getElementByid : Selection via l'ID
+// Deuxième méthode || getElementByid : Selection via l'ID
 //////-------------------------------------------------------
 
 let select2 = document.getElementById('titreNiveau1');
