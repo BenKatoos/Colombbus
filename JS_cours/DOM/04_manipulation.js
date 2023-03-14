3ab92becb5f7bfe2daa5218184b3f8630f738bfc
BenKatoos <122570122+BenKatoos@users.noreply.github.com>
2023-03-14 12:25:11 +0100

    ou

--- JS_cours/DOM/04_manipulation.js
+++ JS_cours/DOM/04_manipulation.js
@@ -10,6 +10,43 @@ function inscription(event) {
     event.preventDefault();
     let info = document.querySelector('#info');
     let erreur = document.querySelectorAll('.erreur');
-    let prenom = event.target.prenom.value;
-    console.log(prenom);
+    let prenom = event.target.prenom.value.trim(); // méthode supprime les espaces aux deux extrémités
+    let nom = event.target.nom.value.trim();
+    let email = event.target.email.value.trim();
+    let mdp = event.target.mdp.value.trim();
+    let confmdp = event.target.confmdp.value.trim();
+
+    // je stock les données de mon formulaire dans un tableau
+    let donnee = [prenom, nom, email, mdp, confmdp];
+    // Si le tableau contient un élément avec chaîne de caractères vides
+    if (donnee.includes('')) {
+        info.innerHTML = `<p class="alert alert-danger">Tous les champs sont requis</p>`;
+        for (let i = 0; i < event.target.length - 1; i++) {
+            if (event.target[i].value === '') {
+                event.target[i].classList.add('error');
+            }
+            else {
+                event.target[i].classList.remove('error');
+            }
+        }
+    }
+    else {
+        if (isNaN(prenom) || isNaN(nom)) {
+            info.innerHTML = `<p class="alert alert-danger"> Le nom ou le prénom est invalide </p>`;
+            if (!isNaN(prenom)) {
+                event.target.prenom.classList.add('error');
+            }
+            if (!isNaN(nom)) {
+                event.target.nom.classList.add('error');
+            }
+            else {
+                event.target.prenom.classList.remove('error');
+                event.target.nom.classList.add('error');
+            }
+        }
+        else {
+
+        }
+    }
 }
+
